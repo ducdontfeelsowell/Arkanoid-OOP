95d41f01aafbbfb3780017f0246bacea5e18afec
Phí Dương Đạt <hn447488@gmail.com>
2025-10-12 15:26:51 +0700

    fix lỗi reset vị trí reset paddle khi mất mạng, thêm giai đoạn chờ phát bóng

--- src/main/java/org/example/arkanoid/input/InputHandler.java
+++ src/main/java/org/example/arkanoid/input/InputHandler.java
@@ -1,21 +1,29 @@
 package org.example.arkanoid.input;
 
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import org.example.arkanoid.config.Constants;
 import org.example.arkanoid.object.Paddle;
+
+import static org.example.arkanoid.config.Constants.isStarted;
 
 public class InputHandler {
     private boolean leftPressed;
     private boolean rightPressed;
     private boolean enterPressed;
     private boolean escapePressed;
+    private boolean spacePressed;
+
 
     public InputHandler(Scene scene) {
         attach(scene);
     }
 
     public void attach(Scene scene) {
+
         scene.setOnKeyPressed(e -> {
             switch (e.getCode()) {
+                case SPACE -> spacePressed = true;
                 case LEFT -> leftPressed = true;
                 case RIGHT -> rightPressed = true;
                 case ENTER -> enterPressed = true;
@@ -29,11 +37,16 @@ public class InputHandler {
                 case RIGHT -> rightPressed = false;
                 case ENTER -> enterPressed = false;
                 case ESCAPE -> escapePressed = false;
+                case SPACE -> spacePressed = false;
             }
         });
     }
 
     public void handleInput(Paddle paddle) {
+
+        if (isSpacePressed()) {
+            isStarted = true;
+        }
         if (isLeftPressed()) {
             paddle.moveLeft();
         } else if (isRightPressed()) {
@@ -55,5 +68,9 @@ public class InputHandler {
 
     public boolean isEscapePressed() {
         return escapePressed;
+    }
+
+    public boolean isSpacePressed() {
+        return spacePressed;
     }
 }
