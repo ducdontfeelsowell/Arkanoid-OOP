aa14e404bb7f78b72b3b053f404988b7a4b6074e
duc <darkvordtmidir@gmail.com>
2025-10-16 23:15:33 +0700

    fixed for ball and paddle collision

--- src/main/java/org/example/arkanoid/config/Constants.java
+++ src/main/java/org/example/arkanoid/config/Constants.java
@@ -7,21 +7,21 @@ public class Constants {
     public final static double SCREEN_HEIGHT = 690;
 
     public final static double DEFAULT_PADDLE_WIDTH = 120;
-    public final static double DEFAULT_PADDLE_HEIGHT = 20;
+    public final static double DEFAULT_PADDLE_HEIGHT = 40;
     public final static double DEFAULT_PADDLE_POSITION_X = SCREEN_WIDTH / 2 - DEFAULT_PADDLE_WIDTH / 2;
     public final static double DEFAULT_PADDLE_POSITION_Y = SCREEN_HEIGHT - 100;
     public final static double DEFAULT_PADDLE_DX = 0;
     public final static double DEFAULT_PADDLE_DY = 0;
-    public final static double DEFAULT_PADDLE_SPEED = 8;
+    public final static double DEFAULT_PADDLE_SPEED = 4;
 
     public final static double DEFAULT_BALL_SIZE = 15;
     public final static double DEFAULT_BALL_POSITION_X = SCREEN_WIDTH / 2 - DEFAULT_BALL_SIZE / 2;
     public final static double DEFAULT_BALL_POSITION_Y = DEFAULT_PADDLE_POSITION_Y - DEFAULT_BALL_SIZE - 5;
-    public final static double DEFAULT_BALL_DX = 5;
-    public final static double DEFAULT_BALL_DY = -5;
-    public final static double DEFAULT_BALL_SPEED = 4;
-    public final static int DEFAULT_BALL_DIRECTION_X = 1;
-    public final static int DEFAULT_BALL_DIRECTION_Y = -1;
+    public final static double DEFAULT_BALL_SPEED = 3;
+    public final static double DEFAULT_BALL_OFFSET = 0.2;
+    public final static double DEFAULT_BALL_OFFSET_CAP = 0.9;
+    public final static double DEFAULT_BALL_DX = 0;
+    public final static double DEFAULT_BALL_DY = -1;
 
     public final static int HIT_POINTS_NORMAL_BRICK = 1;
     public final static int HIT_POINTS_STRONG_BRICK = 3;
@@ -30,7 +30,7 @@ public class Constants {
     public final static double BRICK_WIDTH = 80;
     public final static double BRICK_HEIGHT = 30;
 
-    public final static int INITIAL_LIVES = 3;
+    public final static int INITIAL_LIVES = 99;
     public final static int POINTS_PER_BRICK = 10;
 
     public static boolean isStarted = false;
