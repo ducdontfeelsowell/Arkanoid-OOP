dd5df007b87dc1c9bfb96d0ae290850fdc959ea1
duc <darkvordtmidir@gmail.com>
2025-10-26 22:49:51 +0700

    immortal should be immortal

--- src/main/java/org/example/arkanoid/logic/destroyRegion.java
+++ src/main/java/org/example/arkanoid/logic/destroyRegion.java
@@ -3,6 +3,7 @@ package org.example.arkanoid.logic;
 import org.example.arkanoid.config.Constants;
 import org.example.arkanoid.object.Brick.Brick;
 import org.example.arkanoid.object.Brick.ExplodeBrick;
+import org.example.arkanoid.object.Brick.InfBrick;
 
 public class destroyRegion {
     public static int destroyer(Brick[][] bricks, int row, int col, int points) {
@@ -12,8 +13,13 @@ public class destroyRegion {
             for (int c = Math.max(0, col - 1); c <= Math.min(cols - 1, col + 1); c++) {
                 Brick brick = bricks[r][c];
                 if (brick != null && !brick.isDestroyed()) {
-                    brick.setHitPoints(0);
-                    brick.setDestroyed(true);
+                    if (brick instanceof InfBrick) {
+                        continue;
+                    } else {
+                        brick.setHitPoints(0);
+                        brick.setDestroyed(true);
+                    }
+
                     points += Constants.POINTS_PER_BRICK;
                     if (brick instanceof ExplodeBrick) {
                         destroyer(bricks, r, c, points);
