9dfd5b8ec6c843d17d276ba092ab48e74b435676
duc <darkvordtmidir@gmail.com>
2025-11-08 21:10:15 +0700

    logic modification, multithread

--- src/main/java/org/example/arkanoid/logic/BreakBrick.java
+++ src/main/java/org/example/arkanoid/logic/BreakBrick.java
@@ -4,36 +4,52 @@ import org.example.arkanoid.config.Constants;
 import org.example.arkanoid.game.EffectManager;
 import org.example.arkanoid.game.GameManager;
 import org.example.arkanoid.game.ItemManager;
+import org.example.arkanoid.game.SoundManager;
 import org.example.arkanoid.object.Brick.Brick;
+import org.example.arkanoid.object.Brick.InfBrick;
 
 public class BreakBrick {
 
     public static void breakIt(Brick[][] bricks, Brick brick, GameManager gm, ItemManager im, int row, int col) {
+        // --- SỬA ĐỔI: Di chuyển âm thanh và thêm logic cho case 3 ---
         switch (brick.getType()) {
-            case 1, 2 -> {
+            case 1, 2: // Gạch thường và gạch mạnh
+                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_PADDLE_HIT);
                 brick.takeHit();
                 if (brick.isDestroyed()) {
                     gm.setScore(gm.getScore() + Constants.POINTS_PER_BRICK * brick.getType());
 
-                    // THÊM MỚI: Spawn item khi gạch bị phá
                     if (im != null) {
                         im.spawnItem(brick);
                     }
                     EffectManager.getInstance().spawnBrickDebris(brick);
                 }
-            }
+                break; // Thêm break
 
-            case 4 -> {
+            case 3: // Gạch bất tử (InfBrick)
+                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_WALL_HIT); // Phát âm thanh va tường
+
+                // Kích hoạt animation
+                if (brick instanceof InfBrick) {
+                    ((InfBrick) brick).triggerAnimation();
+                }
+                break; // Thêm break
+
+            case 4: // Gạch nổ
+                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_EXPLOSION);
+
                 double centerX = brick.getX() + brick.getWidth() / 2;
                 double centerY = brick.getY() + brick.getHeight() / 2;
                 EffectManager.getInstance().spawnExplosion(centerX, centerY);
-                EffectManager.getInstance().shakeScreen(5, 150_000_000L);                int points = 0;
+                EffectManager.getInstance().shakeScreen(5, 150_000_000L);
+                int points = 0;
                 brick.takeHit();
                 points = DestroyRegion.destroyer(bricks, row, col, points);
                 gm.setScore(gm.getScore() + points);
-            }
+                break; // Thêm break
 
-            default -> {}
+            default: {}
         }
+        // --- KẾT THÚC SỬA ĐỔI ---
     }
 }
--- src/main/java/org/example/arkanoid/logic/HandleBrickCollision.java
+++ src/main/java/org/example/arkanoid/logic/HandleBrickCollision.java
@@ -44,47 +44,6 @@ public class HandleBrickCollision {
             ball.reverseY();
         }
 
-        // --- SỬA ĐỔI: Di chuyển âm thanh và thêm logic cho case 3 ---
-        switch (brick.getType()) {
-            case 1, 2: // Gạch thường và gạch mạnh
-                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_PADDLE_HIT);
-                brick.takeHit();
-                if (brick.isDestroyed()) {
-                    gm.setScore(gm.getScore() + Constants.POINTS_PER_BRICK * brick.getType());
-
-                    if (im != null) {
-                        im.spawnItem(brick);
-                    }
-                    EffectManager.getInstance().spawnBrickDebris(brick);
-                }
-                break; // Thêm break
-
-            case 3: // Gạch bất tử (InfBrick)
-                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_WALL_HIT); // Phát âm thanh va tường
-
-                // Kích hoạt animation
-                if (brick instanceof InfBrick) {
-                    ((InfBrick) brick).triggerAnimation();
-                }
-                break; // Thêm break
-
-            case 4: // Gạch nổ
-                SoundManager.getInstance().playSoundEffect(Constants.PATH_TO_SOUND_EXPLOSION);
-
-                double centerX = brick.getX() + brick.getWidth() / 2;
-                double centerY = brick.getY() + brick.getHeight() / 2;
-                EffectManager.getInstance().spawnExplosion(centerX, centerY);
-                EffectManager.getInstance().shakeScreen(5, 150_000_000L);
-                int points = 0;
-                brick.takeHit();
-                points = DestroyRegion.destroyer(bricks, row, col, points);
-                gm.setScore(gm.getScore() + points);
-                break; // Thêm break
-
-            default: {}
-        }
-        // --- KẾT THÚC SỬA ĐỔI ---
-
         BreakBrick.breakIt(bricks, brick, gm, im, row, col);
     }
 }
