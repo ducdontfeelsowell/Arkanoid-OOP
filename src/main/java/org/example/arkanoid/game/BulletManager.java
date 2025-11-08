064e0bc41c32aab34235d96853fbc61fe8a4bf10
vutienmanh26 <24020226@vnu.edu.vn>
2025-11-08 19:26:51 +0700

    commit lại

--- src/main/java/org/example/arkanoid/game/BulletManager.java
+++ src/main/java/org/example/arkanoid/game/BulletManager.java
@@ -5,6 +5,7 @@ import org.example.arkanoid.object.Brick.Brick;
 import org.example.arkanoid.object.Bullet;
 import org.example.arkanoid.config.Constants;
 import org.example.arkanoid.game.SoundManager; // THÊM MỚI
+import org.example.arkanoid.game.EffectManager;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -78,6 +79,16 @@ public class BulletManager {
                         if (brick.isDestroyed()) {
                             gm.setScore(gm.getScore() + brick.getScore());
                             im.spawnItem(brick);
+                            if (brick.getType() == 4) {
+                                // Nếu là gạch nổ, tạo vụ nổ + rung
+                                double centerX = brick.getX() + brick.getWidth() / 2;
+                                double centerY = brick.getY() + brick.getHeight() / 2;
+                                EffectManager.getInstance().spawnExplosion(centerX, centerY);
+                                EffectManager.getInstance().shakeScreen(5, 150_000_000L);
+                            } else {
+                                // Nếu là gạch thường, tạo mảnh vỡ
+                                EffectManager.getInstance().spawnBrickDebris(brick);
+                            }
                         }
 
                         hit = true;
